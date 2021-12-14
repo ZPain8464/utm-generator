b915f9b028cf405a42a89f12d3fb5f49783c7309
Zach Painter <z.painter45@gmail.com>
2021-12-13 18:08:27 -0600

    added google sign-in to retrieve user profile data

--- js/index.js
+++ js/index.js
@@ -1,11 +1,89 @@
 // https://ga-dev-tools.web.app/campaign-url-builder/
 /**
  * TODO: Handle export data function
+ *
+ * TODO: Add 'Content' field --> (not required)
+ * TODO: Confirm what fields we definitely need and don't need with Marketing
  */
+
+// Removed secrets for now
+
+ const authorizeButton = document.getElementById('authorize_button');
+ const signoutButton = document.getElementById('signout_button');
+ const formField = document.getElementById("form_container");
+ const urlField = document.getElementById("generated_url-container");
+ function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+    
+  }
+
+  function initClient() {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(function () {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state.
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    }, function(error) {
+      appendPre(JSON.stringify(error, null, 2));
+    });
+  }
+
+  function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+      authorizeButton.style.display = 'none';
+      signoutButton.style.display = 'block';
+      formField.style.display = "block";
+      urlField.style.display = "block";
+    } else {
+      authorizeButton.style.display = 'block';
+      signoutButton.style.display = 'none';
+    }
+  }
+
+  function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+
+  /**
+   *  Sign out the user upon button click.
+   */
+  function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+    formField.style.display = "none";
+    urlField.style.display = "none";
+  }
+
+  function listMajors() {
+    gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+      range: 'Class Data!A2:E',
+    }).then(function(response) {
+      var range = response.result;
+      if (range.values.length > 0) {
+        appendPre('Name, Major:');
+        for (i = 0; i < range.values.length; i++) {
+          var row = range.values[i];
+          // Print columns A and E, which correspond to indices 0 and 4.
+          appendPre(row[0] + ', ' + row[4]);
+        }
+      } else {
+        appendPre('No data found.');
+      }
+    }, function(response) {
+      appendPre('Error: ' + response.result.error.message);
+    });
+  }
+
 const getFormData = (event) => {
     event.preventDefault();
-    const fullName = document.getElementById("full_name").value;
-    const email = document.getElementById("email").value;
     const url = document.getElementById("base_url").value;
     const source = document.getElementById("source").value;
     const searchTerm = document.getElementById("search_term").value;
@@ -13,16 +91,16 @@ const getFormData = (event) => {
     const campaign = document.getElementById("campaign_name").value;
     const promoType = document.getElementById("promotion_type")
     .options[document.getElementById('promotion_type').selectedIndex].text;
+    const purpose = document.getElementById('purpose').value;
     
     const formData = {
-        fullName,
-        email,
         url, 
         source, 
         searchTerm,
         medium, 
         campaign, 
-        promoType
+        promoType, 
+        purpose
     }
 
     handleFormData(formData);
@@ -38,36 +116,35 @@ const handleFormData = (f) => {
     const campaignParam = handleCampaign(f.campaign);
     const mediumParam = handleMedium(f.medium);
     const termParam = handleTerm(f.searchTerm);
-    
+    const purpose = f.purpose
+
     if(termParam === null && promoParam === null) {
         let urlString = baseUrl + '?' + sourceParam + '&' + campaignParam + '&' + mediumParam;
-        const formattedUrl = urlString.toLowerCase();
-        const sheetData = {name: f.fullName, email: f.email, url: formattedUrl}
+        const formattedUrl = urlString
+        const sheetData = {url: formattedUrl, purpose: purpose}
         exportData(sheetData);
         return generateUrl(formattedUrl);
     }
 
     if(termParam === null) {
-        console.log("term null");
         let urlString = baseUrl + '?' + sourceParam + '&' + campaignParam + '&' + mediumParam + '&' + promoParam;
-        const formattedUrl = urlString.toLowerCase();
-        const sheetData = {name: f.fullName, email: f.email, url: formattedUrl}
+        const formattedUrl = urlString
+        const sheetData = {url: formattedUrl, purpose: purpose}
         exportData(sheetData);
         return generateUrl(formattedUrl); 
     }
     
     if(promoParam === null) {
-        console.log("promo null");
         let urlString = baseUrl + '?' + sourceParam + '&' + campaignParam + '&' + mediumParam + '&' + termParam;
-        const formattedUrl = urlString.toLowerCase();
-        const sheetData = {name: f.fullName, email: f.email, url: formattedUrl}
+        const formattedUrl = urlString
+        const sheetData = {url: formattedUrl, purpose: purpose}
         exportData(sheetData);
         return generateUrl(formattedUrl); 
     }
 
     let urlString = baseUrl + '?' + sourceParam + '&' + campaignParam + '&' + mediumParam + '&' + termParam + '&' + promoParam;
-    const formattedUrl = urlString.toLowerCase();
-    const sheetData = {name: f.fullName, email: f.email, url: formattedUrl}
+    const formattedUrl = urlString
+    const sheetData = {url: formattedUrl, purpose: purpose}
     exportData(sheetData);
     generateUrl(formattedUrl);
 } 
@@ -92,19 +169,34 @@ const handlePromo = (promo) => {
 }
 
 const handleCampaign = (c) => {
-    const formatData = c.replace(/ /g, "_");
+  const words = c.split(' ');
+  for (let i = 0; i < words.length; i++) {
+     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  const addCaps = words.join(" ");
+    const formatData = addCaps.replace(/ /g, "_");
     const parameter =  'utm_campaign=' + formatData;
     return parameter;
 }
 
 const handleSource = (s) => {
-    const formatData = s.replace(/ /g, "_");
+  const words = s.split(' ');
+  for (let i = 0; i < words.length; i++) {
+     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  const addCaps = words.join(" ");
+    const formatData = addCaps.replace(/ /g, "_");
     const parameter =  'utm_source=' + formatData;
     return parameter;
 }
 
 const handleMedium = (m) => {
-    const formatData = m.replace(/ /g, "_");
+  const words = m.split(' ');
+  for (let i = 0; i < words.length; i++) {
+     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  const addCaps = words.join(" ");
+    const formatData = addCaps.replace(/ /g, "_");
     const parameter =  'utm_medium=' + formatData;
     return parameter;
 }
@@ -113,15 +205,29 @@ const handleTerm = (t) => {
     if(t === "") {
         return null;
     }
-    const formatData = t.replace(/ /g, "_");
+    const words = t.split(' ');
+  for (let i = 0; i < words.length; i++) {
+     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  const addCaps = words.join(" ");
+    const formatData = addCaps.replace(/ /g, "_");
     const parameter =  'utm_term=' + formatData
     return parameter;
 }
 
 ///// Export to Google Sheets /////
 const exportData = (data) => {
-    // TODO: Get Date(), export to GoogleSheets
-    console.log(data)
+  //TODO: Handle Date();
+  const currentUser = gapi.auth2.getAuthInstance().currentUser.get();
+  const fullName = currentUser.getBasicProfile().getName();
+  const email = currentUser.getBasicProfile().getEmail();
+
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1qNGvs-V-EOgpuWprdABIGRaZASLihNc0GKX6kVZmBIU',
+    range: 'UTM Link Tracking Sheet!A1:A2',
+  }).then(function(response) {
+    console.log(response.range);
+  })
 }
 
 ///// Button Functions /////
